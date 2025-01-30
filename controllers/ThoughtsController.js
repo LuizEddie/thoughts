54ad968b283b1757fb0f7d33506cd99c38b6c8a8
Luiz Eduardo da Silva Magalhaes <contato.luiz1997@gmail.com>
2025-01-30 11:00:47 -0300

    feat: Adicionado edição do sistema

--- controllers/ThoughtsController.js
+++ controllers/ThoughtsController.js
@@ -2,15 +2,109 @@ const Thought = require('../models/Thought');
 const User = require('../models/User');
 
 module.exports = class ThoughtsController {
-    static async dashboard(req, res){
-        res.render('thoughts/dashboard');
+    static async dashboard(req, res) {
+        const userId = req.session.userid;
+
+        if(['', undefined, null].includes(userId)){
+            res.redirect('/login');
+            return;
+        }
+
+        const user = await User.findOne({
+            where: {id: userId},
+            include: Thought,
+            plain: true
+        });
+
+        if(!user){
+            res.redirect('/login');
+            return;
+        }
+
+        let emptyThoughts = false;
+
+        const thoughts = user.Thoughts.map((result)=> result.dataValues);
+
+        if(thoughts.length === 0){
+            emptyThoughts = true;
+        }
+
+        res.render('thoughts/dashboard', {thoughts, emptyThoughts});
     }
 
-    static createThought(req, res){
+    static createThought(req, res) {
         res.render('thoughts/create');
     }
 
-    static async showThoughts(req, res){
+    static async createThoughtPost(req, res) {
+        const data = {
+            title: req.body.title,
+            UserId: req.session.userid
+        }
+
+        try {
+            await Thought.create(data);
+
+            req.flash('message', 'Pensamento criado com sucesso!');
+
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard');
+            })
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    static async showThoughts(req, res) {
         res.render('thoughts/home');
+    }
+
+    static async removeThought(req, res){
+        const id = req.body.id;
+        const userId = req.session.userid;
+
+        try{
+            await Thought.destroy({where: {id:id, UserId: userId}});
+            
+            req.flash('message', 'Pensamento removido com sucesso!');
+
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard');
+            });
+        }catch(e){
+            console.log(e);
+        }
+    
+        
+    }
+
+    static async updateThought(req, res){
+        const id = req.params.id;
+
+        const thought = await Thought.findOne({where: {id: id}, raw: true});
+
+        res.render('thoughts/edit', {thought});
+    }
+
+    static async updateThoughtPost(req, res){
+        const {id, title} = req.body;
+
+        const data = {
+            id,
+            title
+        }
+
+        try{
+            await Thought.update(data, {where: {id: id}});
+
+            req.flash('message', 'Pensamento atualizado com sucesso!');
+
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard');
+            })
+        }catch(e){
+            console.log(e);
+        }
+
     }
 }
--- routes/thoughtsRoutes.js
+++ routes/thoughtsRoutes.js
@@ -6,7 +6,11 @@ const ThoughtsController = require('../controllers/ThoughtsController');
 const checkAuth = require('../helpers/auth').checkAuth;
 
 router.get('/dashboard', checkAuth, ThoughtsController.dashboard);
+router.post('/remove', checkAuth, ThoughtsController.removeThought);
+router.get('/edit/:id', checkAuth, ThoughtsController.updateThought);
+router.post('/edit', checkAuth, ThoughtsController.updateThoughtPost);
 router.get('/add', checkAuth, ThoughtsController.createThought);
+router.post('/add', checkAuth, ThoughtsController.createThoughtPost);
 router.get('/', ThoughtsController.showThoughts);
 
 module.exports = router;
